0d24ad3ced7336378a0fbe8ab0674f1faac1ed97
arbazgormani <arbaz.khan.gormani@gmail.com>
2024-12-05 13:33:34 +0100

    update bash script

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-echo "Running the ETL pipeline........"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+BOLD="\033[1m"
+RESET="\033[0m"
+
+echo -e "${BOLD}${BLUE}========================================="
+echo -e "         Starting the ETL Pipeline        "
+echo -e "=========================================${RESET}"
+echo -e "${YELLOW}Running the ETL pipeline...${RESET}"
+
 python3 project/pipeline.py
 
 # Verify if ETL pipeline executed successfully
 if [ $? -eq 0 ]; then
-    echo "ETL pipeline executed successfully. Running test cases..."
+    echo -e "${GREEN}${BOLD}✔ ETL pipeline executed successfully!${RESET}"
+    echo -e "${YELLOW}Running test cases...${RESET}"
     python3 project/test.py
+    echo -e "${GREEN}${BOLD}✔ Test cases executed successfully!${RESET}"
 else
-    echo "ETL pipeline failed. Skipping test cases."
+    echo -e "${RED}${BOLD}✘ ETL pipeline failed.${RESET}"
+    echo -e "${RED}Skipping Test cases.${RESET}"
 fi
--- project/test.sh
+++ project/test.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-echo "Running the Test cases of the pipeline........"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+BOLD="\033[1m"
+RESET="\033[0m"
+
+echo -e "${BOLD}${YELLOW}========================================="
+echo -e "         Starting The Test Cases                 "
+echo -e "=========================================${RESET}"
+echo -e "${BLUE}Running test cases...${RESET}"
 python3 project/test.py
-echo "Test cases done."
+
+# Verify if test cases executed successfully
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}${BOLD}✔ Test cases executed successfully!${RESET}"
+else
+    echo -e "${RED}${BOLD}✘ Test cases failed.${RESET}"
+fi
